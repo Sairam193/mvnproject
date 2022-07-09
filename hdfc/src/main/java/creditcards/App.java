595d08a9540f45aec7ee6c1c17215d987bdef7cd
Sairam193 <sai319d365@gmail.com>
2022-07-09 19:37:34 +0530

    my fist project

---
+++ hdfc/src/main/java/creditcards/App.java
@@ -0,0 +1,13 @@
+package creditcards;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
